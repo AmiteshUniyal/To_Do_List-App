c7e2b37117795c83732a762a3b9e278d4d47afed
Amitesh Uniyal <amiteshuniyalz7@gmail.com>
2024-07-27 23:27:06 +0530

    pushing all files of todo-list

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import {AppBar, Toolbar, Typography, Box, Paper, Container} from '@mui/material';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos([...todos, { ...todo, completed: false }]); // Spread todo object with completed:false
+  };
+
+  const deleteTodo = (index) => {
+    setTodos(todos.filter((_, i) => i !== index));
+  };
+
+  const toggleTodo = (index) => {
+    setTodos(
+      todos.map((todo, i) =>
+        i === index ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  return (
+    <>
+      <AppBarComponent/>
+      <Container maxWidth="sm">
+        <Box mt={4}>
+          <Paper elevation={3} style={{ padding: '1rem' }}>
+            <Typography variant="h4" align="center" gutterBottom>
+              To-Do List
+            </Typography>
+            <TodoForm addTodo={addTodo} />
+            <TodoList todos={todos} deleteTodo={deleteTodo} toggleTodo={toggleTodo} />
+          </Paper>
+        </Box>
+      </Container>
+    
+    </>
+  );
+};
+function AppBarComponent() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>Amitesh Uniyal's -- To-Do List</Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+
+
+export default App;
---
+++ src/components/TodoList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { List } from '@mui/material';
+import TodoItem from './TodoItem';
+
+const TodoList = ({ todos, deleteTodo, toggleTodo }) => {
+  return (
+    <List>
+      {todos.map((todo, index) => (
+        <TodoItem
+          key={index} 
+          todo={todo}
+          index={index}
+          deleteTodo={deleteTodo}
+          toggleTodo={toggleTodo}
+        />
+      ))}
+    </List>
+  );
+};
+
+export default TodoList;
